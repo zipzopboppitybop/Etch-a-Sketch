26efd9991110cd1e73eaa13ccba79256ae66dfa5
Brian Washington <bw49078@gmail.com>
2022-06-21 15:21:06 -0500

    added hover effect on cells

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const grid = document.getElementById("container");
+let grid = document.getElementById("container");
+
+function makegrid() {
+for (let i =0; i < 256; i++) {
+    let cell = document.createElement("div");
+    cell.className = "cell"
+    grid.appendChild(cell);
+    cell.addEventListener("mouseover", function() {
+        cell.style.backgroundColor = "black"
+    })
+}
+}
+
+makegrid()
